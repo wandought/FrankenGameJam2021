38f457822de3aca8314115266fa597a7e55d40da
Feuerfalke <feuerfalke2.0@gmail.com>
2021-11-05 21:45:22 +0100

    added example tower
    This is only a visual example, the code and structure are merely for demonstration purposes xoxo

--- Assets/Scripts/exampleTowerScript.cs
+++ Assets/Scripts/exampleTowerScript.cs
@@ -5,34 +5,36 @@ using UnityEngine;
 public class exampleTowerScript : MonoBehaviour
 {
 
-  [SerializeField] private Vector3[] bulletSpawns;
-  [SerializeField] private GameObject projectile;
-  [SerializeField] private float shootEveryXSeconds;
-  private float counter;
+			[SerializeField] private GameObject[] bulletSpawns;
+			[SerializeField] private GameObject projectile;
+			[SerializeField] private float shootEveryXSeconds;
+			private float counter;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    if (bulletSpawns == null)
-    {
-      Debug.Log("ERROR: bulletSpawns[] empty!");
-    }
+			// Start is called before the first frame update
+			void Start()
+			{
+						if (bulletSpawns == null)
+						{
+									Debug.Log("ERROR: bulletSpawns[] empty!");
+						}
 
-    counter = shootEveryXSeconds;
+						counter = shootEveryXSeconds;
 
-  }
+			}
 
-  // Update is called once per frame
-  void Update()
-  {
-    counter -= Time.deltaTime;
-    if (counter <= 0f)
+			// Update is called once per frame
+			void Update()
+			{
+						counter -= Time.deltaTime;
+						if (counter <= 0f)
 
-      foreach (Vector3 bs in bulletSpawns)
-      {
-        Instantiate(projectile, bs, Quaternion.Euler(new Vector3(0f, 0f, 0f)));
-      }
-    counter += shootEveryXSeconds;
-  }
+									foreach (GameObject bs in bulletSpawns)
+									{
+												Debug.Log("hallo");
+												Instantiate(projectile, bs.transform.position, this.transform.rotation);
+												counter += shootEveryXSeconds;
+									}
+						
+			}
 }
 
