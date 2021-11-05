4b747565399b23a318fbb90ec133c7baa941f101
Johannes Gattig <johannes.gattig@redheads.de>
2021-11-05 15:21:55 +0100

    removed stupid indent

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-            Debug.Log("Test");   
+        Debug.Log("Test");   
     }
 
     // Update is called once per frame
